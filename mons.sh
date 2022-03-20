c7a10332e731f8cb9a3201255980372b695f5fc9
bielids <62386575+bielids@users.noreply.github.com>
2022-03-20 18:48:50 +0000

    Update mons.sh

--- mons.sh
+++ mons.sh
@@ -17,12 +17,11 @@ printUsage() {
 # gather args
 args=$(getopt -l "arrangement:,laptop:,enabled:,primary:,debug,help" -o "a:l:e:p:hd" -- "$@")
 
-if ! [ $(eval set -- "$args") ] ; then
-    printUsage
-    exit 1
-fi
+#if ! [ $(eval set -- "$args") ] ; then
+#    printUsage
+#    exit 1
+#fi
 
-echo fu
 while [ $# -ge 1 ]; do
         case "$1" in
                 --)
@@ -64,18 +63,17 @@ debug() {
     fi
 }
 
-debug test
-
 if [[ ! "${arrangement}" ]] || [[ ! "${laptop}" ]] ||[[ ! "${enabled}" ]] ||[[ ! "${primary}" ]]; then
     printUsage
     exit 1
 fi
 
 laptopDP='eDP-1'
-for monitor in $(xrandr | grep conn | grep -ivE ".*connected \(|${laptopDP}" | awk '{print $1}' | sort); do
+for monitor in $(xrandr | grep ' conn' | grep -iv "${laptopDP}" | awk '{print $1}' | sort); do
     monitors+=("${monitor}")
 done
 
+debug "Monitor arrangenent: $(if [[ ${laptop} == "on" ]]; then printf "${laptopDP} "; fi)$(IFS=','; for id in ${arrangement}; do printf "${monitors[$((${id} -1))]} "; done)"
 count=0
 
 debug "Starting loop"
@@ -86,37 +84,48 @@ for id in ${arrangement}; do
     fi
     (( count++ ))
     if [[ ! ${prev} ]]; then
-        debug "No previous display set"
+        debug "No previous display set, setting laptop display options"
         cmd="xrandr --output ${laptopDP}"
         if [[ ${laptop} == "on" ]]; then
+            debug "Laptop mode set to auto"
             cmd+=" --auto"            
-        elif ${enabled} -gt 0 ]]; then
+        elif [[ ${enabled} -gt 0 ]]; then
+            debug "Laptop mode set to off"
             cmd+=" --off"
         else
+            debug "Laptop mode set to auto"
             cmd+=" --auto"
         fi
         if [[ ${enabled} -eq 0 ]]; then
+            debug "No other displays enabled, setting laptop to primary"
             cmd+=" --primary"
-            break
+#            break
         fi
     fi
     if [[ ${id} -gt ${#monitors[@]} ]]; then
-        echo "skipping monitor ${id}"
+        echo "skipping monitor ${id} because only ${#monitors[@]} monitors found"
         continue
     fi
+    monitor_name=${monitors[$((${id} -1))]}
+    debug "Setting options for monitor ${monitor_name}"
     if [[ ${id} -eq ${primary} ]] && [[ ! ${disable} ]]; then
-        cmd+=" --output ${monitors[$((${id} - 1))]} --primary"
+        debug "${monitor_name} set as primary display"
+        cmd+=" --output ${monitor_name} --primary"
     else
-        cmd+=" --output ${monitors[$((${id} - 1))]}"
+        cmd+=" --output ${monitor_name}"
     fi
     if [[ ${prev} ]]; then
+        debug "Found previously set display, setting ${monitor_name} to the right of ${monitors[$((${prev} - 1))]}"
         cmd+=" --right-of ${monitors[$((${prev} - 1))]}"
     elif [[ ${laptop} -eq "on" ]]; then
+        debug "Previous display was laptop display, setting ${monitor_name} to the right of ${laptopDP}"
         cmd+=" --right-of ${laptopDP}"
     fi
     if [[ ! ${disable} ]]; then
+        debug "${monitor_name} set to auto mode"
         cmd+=" --auto"
     else
+        debug "${monitor_name} set to off mode"
         cmd+=" --off"
     fi
     prev=${id}
@@ -124,5 +133,5 @@ done
 
 # run the command
 echo "RUNNING ${cmd}"
-read
+printf "Press ENTER to continue"; read
 eval "${cmd}" 
